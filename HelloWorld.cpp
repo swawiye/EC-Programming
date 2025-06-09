51a72e813a7836688df1ce603d1e1abc35a1cc9f
swawiye <sovereign.wawiye@gmail.com>
2025-06-09 20:18:36 +0300

    c++ lesson 1

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+// This is my first C++ program
+#include <iostream> // header file library
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl; 
+    return 0; // indicate that the program ended successfully
+}
+
