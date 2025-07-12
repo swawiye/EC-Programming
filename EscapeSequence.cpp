2f7e605d42860298feda1423e8ff62b566765428
swawiye <sovereign.wawiye@gmail.com>
2025-07-12 15:01:49 +0300

    escape sequences

---
+++ EscapeSequence.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // header file library
+using namespace std;
+
+int main() 
+{
+    // Output statements in the console
+    cout << "Hello World!" << endl;
+    cout << "This is my first C++ output statement.\n\n"; 
+    cout << "Soon, I will learn how to input data into my program" << endl; 
+    cout << "I can use \"double quotes\"." << endl; 
+    cout << "I can use \'single quotes\'." << endl; 
+    cout << "I can use a backslash \\." << endl; 
+    cout << "This is tabbed\t text." << endl; 
+    cout << "System alert sound:\a." << endl; 
+    return 0; // indicate that the program ended successfully
+}
+
+
+  
