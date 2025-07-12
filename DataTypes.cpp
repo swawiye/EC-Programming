d53e7d42fe136f9abb6386e847790a7cf1e0df23
swawiye <sovereign.wawiye@gmail.com>
2025-07-12 15:03:48 +0300

    datatypes

---
+++ DataTypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declare the variables
+    int integerVar = 42; 
+    float floatVar = 3.142;
+    double doubleVar = 2.45675869;
+    char charVar = 'A';
+    bool boolVar = true;
+    string stringVar = "Hello World!";
+
+    // Output the values of the variables
+    cout << "Integer: " << integerVar << endl;
+    cout << "Float: " << floatVar << endl;
+    cout << "Double: " << doubleVar << endl;
+    cout << "Character: " << charVar << endl;
+    cout << "Boolean: " << boolVar << endl;
+    cout << "String: " << stringVar << endl;
+
+    return 0;
+}
