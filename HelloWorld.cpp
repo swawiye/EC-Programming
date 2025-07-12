f1264d2e35ec13ac7ba2884e2a956d17145b847a
swawiye <sovereign.wawiye@gmail.com>
2025-07-12 14:10:12 +0300

    restarting the set up

--- HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-// This is my first C++ program
-#include <iostream> // header file library
-using namespace std;
-int main() {
-    cout << "Hello, World!" << endl; 
-    return 0; // indicate that the program ended successfully
-}
-
-
