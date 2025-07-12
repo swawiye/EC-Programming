e3d0523eda1297f0c53581293eca3e9970e537c2
swawiye <sovereign.wawiye@gmail.com>
2025-07-12 14:47:59 +0300

    usage file

---
+++ Usage.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "You entered" + number << endl;
+    cerr << "This is an example of an error message";
+    //cout << "You entered " << number << endl; //'<<' used when combining outputs mixing strings and numbers
+    //cerr << "This is an example of an error message" << endl;
+}
